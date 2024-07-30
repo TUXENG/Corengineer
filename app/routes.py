c2ed3641ea8324094188f6789fdb116c5bc95152
Miguel Hernández <ingmiguelhernandezarrieta@gmail.com>
2024-07-30 12:39:12 -0500

    agregado de welcome y ver más

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,29 @@
-from flask import render_template, request, redirect, url_for
-from . import app
+import json
+import os
+from flask import current_app as app
+from flask import render_template
+
+
 
 @app.route('/')
 def home():
-    return "Hola, Mundo!"
+    json_path = os.path.join(app.root_path, 'static/json', 'index_cards.json' )
+
+    with open(json_path, 'r', encoding='utf-8') as f:
+        cards =json.load(f)
+
+    return render_template('index.html', cards=cards)
+
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
