8ba19adc82d5412b850dcc5e5894179bb6064525
Miguel Hernández <ingmiguelhernandezarrieta@gmail.com>
2024-07-31 12:08:27 -0500

    Animación boton buscar

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const searchIcon = document.querySelector('.header__search-ico');
     const searchField = document.querySelector('.header__search-textfield');
-
+    
+    
     searchIcon.addEventListener('click', function () {
         // Toggle visibility of the search field
-        if (searchField.style.display === 'block') {
-            searchField.style.display = 'none';
-            searchField.style.opacity = '0';
-        } else {
-            searchField.style.display = 'block';
-            searchField.style.opacity = '1';
-            searchField.focus();
-        }
+        if (searchField.classList.contains('show')) {
+                searchField.classList.remove('show');
+                searchIcon.style.opacity = '1';
+            } else {
+                searchField.classList.add('show');
+                searchIcon.style.opacity = '0.7';
+                searchField.focus();
+            }
     });
 });
+
