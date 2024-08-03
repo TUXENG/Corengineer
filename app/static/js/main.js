6cbb184316e10eb0eee8d1d51456e56585088509
Miguel Hernández <ingmiguelhernandezarrieta@gmail.com>
2024-08-03 00:56:37 -0500

    Header para movil listo. Falta para table y escritorio.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,6 @@
 import searchModule from './modules/searchModule.js';
-import displayMenu from './modules/displayMenu.js';
 
 
 document.addEventListener('DOMContentLoaded', function () {
     searchModule.init();
-    displayMenu.init();
 });
