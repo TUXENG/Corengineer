ca9411922cf634e38ba17a5497bad95b58d1474a
Miguel Hernández <ingmiguelhernandezarrieta@gmail.com>
2024-08-06 13:21:21 -0500

    terminadas las tarjetas en el index. Iniciamos footer

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,6 +1,7 @@
 import searchModule from './modules/searchModule.js';
-
+import gridModule from './modules/gridModule.js'
 
 document.addEventListener('DOMContentLoaded', function () {
     searchModule.init();
+    gridModule.init(4);
 });
