3a92c184999201e49863112ea3924e2cb145b657
Miguel Hernández <ingmiguelhernandezarrieta@gmail.com>
2024-08-23 00:23:53 -0500

    finalizado anipamción typing y styles del welcome

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,9 +1,11 @@
 import gridModule from './modules/gridModule.js'
 import menuModule from './modules/menuModule.js'
 import headerModule from './modules/headerModule.js'
+import typewriterModule from './modules/typewriterModule.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     gridModule.init(4);
     menuModule.init();
     headerModule.init();
+    typewriterModule.init();
 });
--- app/static/js/modules/typewriterModule.js
+++ app/static/js/modules/typewriterModule.js
@@ -1,24 +1,58 @@
 const typewriterModule = (function () {
-    const text = "INNOVATION";
-    const speed = 100;
-    let index = 0;
+    const textArray = [
+        { text: "WEB", textColor: "#757575", bgColor: "#ff5722" },
+        { text: "MOBILE", textColor: "#757575", bgColor: "#f00000" },
+        { text: "ELECTRONIC", textColor: "#ffffff", bgColor: "#00838f" },
+        { text: "ROBOTIC", textColor: "#ffffff", bgColor: "#e91e63" },
+        { text: "INNOVATION", textColor: "#75757500", bgColor: "#9fef00" }
+    ];
+    const typingSpeed = 120;
+    const erasingSpeed = 100;
+    const newTextDelay = 2000;
+    let typingElement = null;
+    let textArrayIndex = 0;
+    let charIndex = 0;
 
-    function typeWriter() {
-        const typeWriterElement = document.querySelector('welcome__slogan-span');
-        if (index < text.length) {
-            typeWriterElement.textContent += text.charAt(index);
-            index++;
-            setTimeout(typeWriter, speed);
+    function applyStyles() {
+        typingElement.style.color = textArray[textArrayIndex].bgColor;
+        typingElement.style.backgroundColor = textArray[textArrayIndex].textColor;
+    }
+
+    function type() {
+        if (charIndex < textArray[textArrayIndex].text.length) {
+            typingElement.textContent += textArray[textArrayIndex].text.charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingSpeed);
+        } else {
+            if (textArrayIndex < textArray.length - 1) {
+                setTimeout(erase, newTextDelay);
+            }
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            typingElement.textContent = textArray[textArrayIndex].text.substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingSpeed);
+        } else {
+            textArrayIndex++;
+            applyStyles();
+            setTimeout(type, typingSpeed + 1100);
         }
     }
 
     function init() {
-        typeWriter();
+        typingElement = document.querySelector(".welcome__slogan-spam-typing");
+        if (typingElement) {
+            applyStyles(); // Aplica los estilos al iniciar
+            setTimeout(type, newTextDelay);
+        }
     }
 
     return {
         init: init
-    }; 
+    };
 })();
 
 export default typewriterModule;
